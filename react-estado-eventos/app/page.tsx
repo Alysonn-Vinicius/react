dfc235b947c4ff327897e9119d40bce939071165
Alysonn03 <alysonvy03@gmail.com>
2026-06-15 18:00:31 -0300

    exercicios de react, uso do state

--- react-estado-eventos/app/page.tsx
+++ react-estado-eventos/app/page.tsx
@@ -10,11 +10,42 @@ const Home = () => {
         console.log(count)
     }
 
+    const [showArea,setShowArea] = useState(false);
+
+    const [nomeInput, setNomeInput] = useState('');
+
     return (
-        <main className="container mx-auto bg-slate-800 flex justify-center items-center h-screen">
+        <main className="container mx-auto bg-slate-800 flex justify-center items-center h-screen flex-col gap-7">
            <div className="flex flex-col gap-0">
                 {count}
                 <button onClick={() => handleCount(count)} className="bg-blue-600 text-white p-2 rounded-md cursor-pointer">+1</button>
+           </div> 
+
+
+            <button className="p-3 bg-sky-600 text-white border border-white rounded-md cursor-pointer" onClick={()=> setShowArea(!showArea)}>{showArea ? 'Ocultar' : 'Mostrar'} area oculta</button>
+
+            {showArea && 
+            <div className="p-3 bg-sky-600 text-white border border-white rounded-md ">
+                    <h3>Area secreta</h3>
+            </div>
+           }
+
+
+
+           <div>
+                <h3 className="text-xl">State em Campos: "inputs"</h3>
+                <input type="text"
+                placeholder="digite seu nome"
+                className="border p-2 bg-gray-500"
+                value={nomeInput} //nome fica com o valor da variavel
+                onChange={e => setNomeInput(e.target.value)}
+                //setando o valor na variavel atraves do state
+                />
+
+                <div>
+                    o que esta sendo digitado: <br /> 
+                    { nomeInput}
+                </div>
            </div>
 
         </main>
---
+++ treinos/app/exercicio01.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { useState } from "react";
+
+export default function atividade() {
+  const [log, setLog] = useState(false);
+  const [count, setCount] = useState(0);
+  const pessoas = [
+    { id: 1, nome: 'Ana' },
+    { id: 2, nome: 'Carlos' },
+    { id: 3, nome: 'João' }
+  ];
+  const handleAdc = () => {
+    setCount(count + 1);
+  }
+  const handleSub = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      alert('valor minino atingido!')
+    }
+
+  }
+
+
+  return (
+    <div className="container mx-auto p-5 flex flex-col gap-3">
+      <button className="bg-blue-600 p-3 text-white rounded-md cursor-pointer" onClick={() => alert('Olá, React.')}>
+        clique
+      </button>
+
+      <button className="bg-blue-600 p-3 text-white rounded-md cursor-pointer" onClick={() => setLog(!log)}>
+        ver lista
+      </button>
+      <ul>
+        {log && pessoas.map(item => (
+          <li key={item.id}>{item.nome}</li>
+        ))}
+      </ul>
+      <br />
+      <div>
+        <h3 className="text-center mb-3">Contador</h3>
+        <div className="flex gap-3 justify-center">
+          <button className="bg-green-800 p-2 text-white rounded-md font-bold cursor-pointer" onClick={handleSub}>-</button>{count}<button className="bg-green-800 p-2 text-white rounded-md font-bold cursor-pointer" onClick={handleAdc}>+</button>
+        </div>
+      </div>
+    </div>
+  );
+}
