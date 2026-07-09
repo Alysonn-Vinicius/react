90f4311fd9385d857d91f185af3c7bade7cf7dea
Alysonn03 <alysonvy03@gmail.com>
2026-07-09 17:55:24 -0300

    reduce

--- useEffect/app/components/cronometro.tsx
+++ useEffect/app/components/cronometro.tsx
@@ -2,20 +2,26 @@ import { useEffect, useState } from "react";
 
 export const Crono = () => {
     const [segundo, setSegundo] = useState(0);
+    const [rodando, setRodando] = useState(true);
 
-    useEffect(()=> {
-        const intervalo = setInterval(()=>{
-            setSegundo(prev=> prev + 1)
-        }, 1000)
-
-
-        //cleanUp
+    useEffect(() => {
+        if (rodando) {
+            const intervalo = setInterval(() => {
+                setSegundo(prev => prev + 1)
+            }, 1000)
+             //cleanUp
         return () => {
             console.log('limpou o clean up')
             clearInterval(intervalo);
         }
-    }, [])
-    return(
-        <div className="container flex items-center justify-center">segundos...: {segundo}</div>  
+        }
+    }, [rodando])
+    return (
+        <>
+            <div className="container flex items-center justify-center">segundos...: {segundo}</div>
+            <button onClick={() => { setRodando(!rodando) }}>{rodando ? "Pause" : "Rodando"}</button>
+        </>
+
+
     );
 }
--- useEffect/app/components/reducer.tsx
+++ useEffect/app/components/reducer.tsx
@@ -1,7 +1,63 @@
+import { useReducer, useState } from "react";
+import { Item } from "../types/item";
+import { listReducer } from "./reducers/listReducer";
+
+
 export const TesteReduce = () => {
+
+    const [list, setList] = useState<Item[]>([])
+
+    //adc
+    const Adc = (text: string) => {
+        setList([...list, {id: Date.now(), text, done:false}])
+    }
+    //toggleText
+    const toggleItem = (id:number, newText:string) => {
+        setList(
+            list.map((item,index)=> {
+                if(item.id === id){
+                    item.text = newText;
+                }
+                return item;
+            })
+        )
+    }
+
+    //trocar done
+    const toggleDone = (id:number) => {
+        setList(
+            list.map((item)=> {
+                if(item.id === id){
+                    item.done = !item.done;
+                }
+                return item;
+            })
+        )
+    }
+
+    //remove item
+    const removeItem = (id:number) => {
+        setList(list.filter(item => item.id !== id))
+    }
+    /**Reducer são feitos para reduzir, essas 3 funcoes servem para modificiar um unico state, reducer vem para minimizar ou deixar tudo em um unico lugar */
+
+
+    //modo de uso do hook - Reducer
+    const [liste, dispatch] = useReducer(listReducer, [])
+    const handleClick = () => {
+        dispatch({
+            type: 'add',
+            payload: 'novo item'
+        });
+
+        dispatch({
+            type:"EditText",
+            payload: {id: 5, newText: 'novotext'}
+        })
+    }
     return (
         <div>
-            <p>Este é o uso do reduce</p>
+            <button onClick={handleClick}></button>
         </div>
     );
 }
